f1785f757cbbe94738e9d1d6d62084a0bdfe2d00
krishna singh <0.krishna1120@gmail.com>
2025-09-05 16:57:15 +0530

    Enhance Sidebar component with user information display and navigation link for creating posts

--- socio_client/src/components/Sidebar.jsx
+++ socio_client/src/components/Sidebar.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { assets } from "../assets/assets";
+import { assets, dummyUserData } from "../assets/assets";
 import MenuItems from "./MenuItems";
+import { CirclePlus, LogOut } from "lucide-react";
+import { Link } from "react-router-dom";
+import {UserButton,useClerk} from '@clerk/clerk-react'
 const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
   const navigate = useNavigate();
+  const user = dummyUserData;
+  const {signOut} = useClerk();
   return (
     <div
       className={`w-60 xl:w-72 bg-white border-r
@@ -22,7 +27,23 @@ duration-300 ease-in-out`}
         />
         <hr className="border-gray-300 mb-8" />
         <MenuItems setSidebarOpen={setSidebarOpen} />
-
+        <Link to='/create-post' className='flex items-center justify-center gap-2 py-2.5 mt-6 mx-6 rounded-xl bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-700 hover: to-purple-800 active:scale-95 transition text-white cursor-pointer'>
+          <CirclePlus className="w-5 h-5"/>
+          Create Post
+        </Link>
+        
+      </div>
+      <div className="w-full border-t border-gray-200 py-4 px-7 flex items-center justify-between">
+       <div className="flex gap-2 items-center cursor-pointer">
+        <UserButton />
+        <div>
+          <h1 className="text-sm font-medium">
+            {user.full_name}
+          </h1>
+          <p className="text-xs text-gray-500">@{user.username}</p>
+        </div>
+       </div>
+       <LogOut className="w-4.5 text-gray-600 hover:text-gray-900 transition cursor-pointer" onClick={signOut}/>
       </div>
     </div>
   );
