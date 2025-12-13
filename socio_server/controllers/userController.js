2daa5f4248110ac7c43e849346c5b6c414f792e9
krishna singh <0.krishna1120@gmail.com>
2025-12-13 12:38:09 +0530

    feat: implement user data retrieval and update functionality with file upload support

---
+++ socio_server/controllers/userController.js
@@ -0,0 +1,48 @@
+// get user fdata using user id
+
+import User from "../models/user";
+import { upload } from "../configs/multer.js";
+import path from "path";
+import fs from "fs";
+
+export const getUserData = async (req, res) => {
+    try {
+        const {userId} = await req.auth();
+        const user = await User.findById(userId)
+        if(!user) {
+            return res.status(404).json({message: 'User not found'});
+        }
+        res.json(user);
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message: 'Internal Server Error'});
+    }
+}
+// UpdateUser data
+
+export const updateUserData = async (req, res) => {
+    try {
+        const {userId} = await req.auth();
+        const {username, bio, locatio, full_name} = req.body;
+        const temoUser = await User.findById(userId)
+        !username && (username = temoUser.username);
+        if(temoUser.username !== username) {
+            const user = await User.findOne({username});
+            if(user) {
+                return res.status(400).json({message: 'Username already taken'});
+        }
+    }
+        const updatedUser = {
+            username,
+            bio,
+            locatio,
+            full_name
+        }
+        const profile  = req.files.profile && req.files.profile[0];
+        const cover  = req.files.cover && req.files.cover[0];
+        }
+      catch (error) {
+        console.log(error)
+        return res.status(500).json({message: 'Internal Server Error'});
+    }
+}
