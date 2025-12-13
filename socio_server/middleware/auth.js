6ac6267054b4b40ebe32f18b4cb5c964e5475f59
krishna singh <0.krishna1120@gmail.com>
2025-12-13 12:27:34 +0530

    fix: ensure next middleware is called after user authentication check

--- socio_server/middleware/auth.js
+++ socio_server/middleware/auth.js
@@ -4,6 +4,7 @@ export const protect = async(req, res, next) => {
         if(!userId) {
             return res.status(401).json({message: 'Unauthorized'});
         }
+        next()
     } catch (error) {
         return res.status(500).json({message: 'Internal Server Error'});
     }
